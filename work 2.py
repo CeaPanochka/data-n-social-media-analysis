a04a9b168a34e81fea53ace8e1acd6a3015f9bbf
CeaPanochka <titampeach@gmail.com>
2024-06-19 13:35:04 +0300

    Выполнены задания 2 и 3

--- work 2.py
+++ work 2.py
@@ -11,4 +11,5 @@ G = nx.Graph()
 G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8])
 G.add_edges_from([(1, 3), (2, 3), (2, 4), (3, 4), (4, 5), (5, 7), (5, 6), (6, 7), (6, 8)])
 
+# результат [0.15, 0.34, 0.38, 0.47, 0.47, 0.38, 0.34, 0.15]
 print(list(map(lambda x: round(x, 2), nx.eigenvector_centrality(G).values())))
